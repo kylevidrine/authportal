f3a85cbb83258ebd54db8ad159ca866bb82fb245
kylevidrine <kylevidrine@me.com>
2025-08-11 11:28:51 -0500

    Auto commit before docker build

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 cd /volume1/web/n8n || { echo "Folder not found"; exit 1; }
-
-# Stop Docker container first
-sudo docker-compose down
-
 # Add all changes
 git add .
-
 # Commit with a default message if there are changes, otherwise skip
 git commit -m "Auto commit before docker build" || echo "No changes to commit"
-
 # Push to GitHub
 git push origin master || { echo "Git push failed"; exit 1; }
-
 # Restart Docker container with rebuild
+sudo docker-compose down
 sudo docker-compose up -d --build
